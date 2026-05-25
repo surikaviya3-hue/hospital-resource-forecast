0aaabc6a579cd18cbc3bcc6127487a9ccfc189ae
surikaviya3-hue <surikaviya3@gmail.com>
2026-05-25 22:47:31 +0530

    updated frontend api url

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -109,7 +109,7 @@ async function predictResources() {
     }
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://hospital-resource-forecast.onrender.com/predict", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
